f7c806c7a477d9f2f2d179338cd925391108edf6
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2023-06-04 12:25:57 -0300

    Initial project.

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import {ref} from 'vue';
+import Child from './Child.vue';
+
+const name = ref('initial');
+</script>
+
+<template>
+	<h1>{{name}}</h1>
+	<Child @changeName="n => name = n" />
+</template>
---
+++ src/Child.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const emit = defineEmits<{
+	changeName: [name: string];
+}>();
+</script>
+
+<template>
+	<button @click="() => emit('changeName', 'foo')">
+		Change name
+	</button>
+</template>
